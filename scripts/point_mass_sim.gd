3d4da377e7f1ecd74d8fba12c0c43928b1376a09
kennethbchen <kennethbchen@gmail.com>
2025-01-25 18:38:22 -0500

    Moved code that uses point mass sim to other script file

--- scripts/point_mass_sim.gd
+++ scripts/point_mass_sim.gd
@@ -2,64 +2,8 @@ extends Node2D
 
 # https://lisyarus.github.io/blog/posts/soft-body-physics.html
 
+class_name PointMassSim
 
-
-var points: Array[PointMass]
-var constraints: Array[SpringConstraint]
-
-var gravity: Vector2 = Vector2(0,900)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var point_count: int = 20
-	var length: float = 40
-
-	var pos: = Vector2(30, 30)
-	for i in range(point_count):
-		points.append(PointMass.new(pos))
-		pos.x += length
-	
-	points[0].fixed = true
-	points[-1].fixed = true
-	
-	for i in range(len(points) - 1):
-		constraints.append(SpringConstraint.new(points[i], points[i + 1], length, 500))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	
-	_simulate(delta)
-	
-	for constraint in constraints:
-		constraint._resolve_constraint(delta)
-		
-	queue_redraw()
-
-func _simulate(delta: float) -> void:
-	# Gravity
-	for point in points:
-		point.velocity += gravity * delta
-	
-	# Symplectic Euler integration
-	for point in points:
-		
-		if point.fixed:
-			point.velocity = Vector2.ZERO
-			continue
-		
-		point.position += point.velocity * delta
-
-func _draw():
-
-	for i in range(len(constraints)):
-		var color: Color = Color.WHITE
-		draw_line(constraints[i].point_a.position, constraints[i].point_b.position, color, 4)
-		
-	for i in range(len(points)):
-
-		draw_circle(points[i].position, 4, Color.LIGHT_GREEN, 5)
-		
 class PointMass:
 	extends RefCounted
 	
