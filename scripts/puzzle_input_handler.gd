519a6bd105bfbc4305f2e2209dcf796df205adda
kennethbchen <kennethbchen@gmail.com>
2025-04-03 12:32:37 -0400

    Added windup to stab and tweaked animation

--- scripts/puzzle_input_handler.gd
+++ scripts/puzzle_input_handler.gd
@@ -5,9 +5,7 @@ enum STATE {IDLE, COOLDOWN}
 @export var cursor: Node3D
 @export var cursor_particles: CPUParticles3D
 
-var cursor_travel_time: float = 0.2
-
-var cursor_hover_height: float = 1.5
+var cursor_hover_height: float = 0.5
 
 var puzzle: Puzzle
 
@@ -31,19 +29,31 @@ func color_cell(row, col) -> void:
 		
 	current_state = STATE.COOLDOWN
 	var tween = get_tree().create_tween()
-	tween.set_trans(Tween.TRANS_BACK)
 	
+	
+	# Wind up
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(cursor, "position:y", cursor_hover_height + 0.75, 0.08)
+
 	# Attack
-	tween.tween_property(cursor, "position:y", -1.5, cursor_travel_time)
+	
+	# The timing of when the knife contacts the body is slightly before
+	# the timing of when the tween is done, so we need to time things
+	# in parallel like this to make it look seamless
+	tween.set_trans(Tween.TRANS_BACK)
 	tween.tween_callback(func():
 		puzzle.toggle_input_colored(row, col)
 		EventBus.screen_shake_requested.emit(0.75)
+		).set_delay(0.1)
+	tween.parallel().tween_callback(func():
 		cursor_particles.emitting = true
-		)
-
+		).set_delay(0.15)
+	tween.parallel().tween_property(cursor, "position:y", -1.5, 0.2)
+	
 	# Hold
 	tween.set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(cursor, "position:y", cursor_hover_height, 0.5).set_delay(0.2)
+	tween.tween_property(cursor, "position:y", cursor_hover_height, 0.3).set_delay(0.2)
 	
 	# Recovery
 	tween.tween_callback(func():
